5cfafe3b6c299a12eb42431000238ac8ee9ec433
hanasaki <hanasakayui2022@gmail.com>
2025-01-08 01:01:22 +0800

    ✨ Add login form

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="flex w-full h-screen items-center justify-center">
-    <Card class="w-1/2 w-min-[200px] overflow-hidden"> </Card>
+    <Card class="w-1/2 w-min-[200px] overflow-hidden">
+      <Form
+        v-slot="$form"
+        :initialValues
+        @submit="onLogin"
+        class="flex flex-col gap-4 sm:56"
+      >
+        <div class="flex flex-col w-full gap-4">
+          <InputText name="uername" type="text" placeholder="Username" fluid />
+        </div>
+        <div class="flex flex-col w-full gap-4">
+          <InputText name="password" type="text" placeholder="Password" fluid />
+        </div>
+        <Button type="submit" severity="secondary" label="Login" />
+      </Form>
+    </Card>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const initialValues = ref({
+  username: '',
+  password: '',
+})
+
+const onLogin = () => {}
+</script>
 
 <style scoped></style>
